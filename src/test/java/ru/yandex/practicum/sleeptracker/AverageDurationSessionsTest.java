a99a88dcaa09e8f0f388955c04c3d21b22f87afb
Marina <marinka_chaikovsky.bk.ru>
2026-09-06 12:55:28 +0500

    исправление ошибок гитхаба

--- src/test/java/ru/yandex/practicum/sleeptracker/AverageDurationSessionsTest.java
+++ src/test/java/ru/yandex/practicum/sleeptracker/AverageDurationSessionsTest.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class AverageDurationFunctionTest {
+class AverageDurationSessionsTest {
 
     @Test
     void returnZeroWhenNoSessions() {
